fca8a00c9ba8c3063a6eda845c8ba1e8d3f14565
jonathanayalacisneros <jonathanayalacisneros@gmail.com>
2022-11-26 05:59:29 -0800

    tercera

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://JonathanAyalaCisneros.github.io/react-rickMorty/",
+  base: "https://JonathanAyalaCisneros.github.io/rick/",
   plugins: [react()]
 })
